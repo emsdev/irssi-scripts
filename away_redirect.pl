d8fae90b5b8dcf10250f45b830cb0a74efef6fb8
ems <ems@null.net>
2013-01-13 22:06:37 +0200

    final version?

---
+++ away_redirect.pl
@@ -0,0 +1,42 @@
+# REMEMBER TO EDIT LINE 22!
+use strict;
+use warnings;
+
+use Irssi;
+use Irssi::Irc;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "1.0";
+
+%IRSSI = (
+    authors     => "ems",
+    contact     => "ems at freenode",
+    name        => "away_redirect",
+    description => "Redirects msg/hilights when you are set away.",
+    license     => "GPL v2",
+    url         => "http://github.com/emsdev",
+);
+
+sub redirect_message {
+    my ($title, $sender, $content) = @_;
+    Irssi::command("msg -bitlbee phone " . $title . " " . $content); # FIXME
+}
+
+sub public_message {
+    my ($server, $content, $nick) = @_;
+
+    if($server->{usermode_away} == 1 && $content =~ /$server->{nick}/i){
+        redirect_message("[HL]", $nick, $content);
+    }
+}
+
+sub private_message {
+    my ($server, $content, $nick) = @_;
+
+    if($server->{usermode_away} == 1){
+        redirect_message("[MSG]", $nick, $content);
+    }
+}
+
+Irssi::signal_add_last('message public', 'public_message');
+Irssi::signal_add_last('message private', 'private_message');
